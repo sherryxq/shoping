14bdba2d615f6631492e7c49bccc0f1c4e6e0dfa
sherryxq <sherryxq@foxmail.com>
2017-09-21 23:50:09 +0800

    修改前端
    修改前端

---
+++ front/src/components/result.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="result">
+  <label class="mylabel01">下单成功！</label>
+  <div>
+    <p class="prompt">我们的配送员会及时将百香果配送到您的房间，请耐心等待！您可以选择通过扫描配送员的二维码进行微信/支付宝付款，或直接现金支付。感谢您的关注，祝您旅途愉快！</p>
+  </div> 
+  <hr />
+  <div>
+    	<p>了解更多，关注“百福速鲜”微信公众号</p>
+    	<img src="/static//img/baifu.jpg" width="900" height="500" />
+	</div>
+</div>  
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .mylabel01 {
+    font-size: 30px;
+    color: #FF0000;
+    align: center;
+  }
+  
+  .prompt {
+    font-size: 15px;
+  }
+</style>
